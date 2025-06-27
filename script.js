e6f7574afb18e33f90e8c33e01e7d61048ad897b
kaizenacademy <kaizenacademy@gmail.com>
2025-06-27 01:57:40 -0500

    new images

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const cardArray = [
         { name: 'card1', img: 'images/jenny.png' },
         { name: 'card1', img: 'images/jenny.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
+        { name: 'card2', img: 'images/jisoo.png' },
+        { name: 'card2', img: 'images/jisoo.png' },
+        { name: 'card3', img: 'images/lisa.png' },
+        { name: 'card3', img: 'images/lisa.png' },
+        { name: 'card4', img: 'images/rose.png' },
+        { name: 'card4', img: 'images/rose.png' },
         { name: 'card5', img: 'images/success.png' },
         { name: 'card5', img: 'images/success.png' },
         // ...add more pairs as needed
