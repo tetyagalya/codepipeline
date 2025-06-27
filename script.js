13bb0bbf1c0ead0a15378e60fc1dcee2fd08214f
kaizenacademy <kaizenacademy@gmail.com>
2025-06-26 23:46:23 -0500

    jenny image

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
     let cardsWon = [];
 
     const cardArray = [
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card1', img: 'images/distracted.png' },
+        { name: 'card1', img: 'images/jenny.png' },
+        { name: 'card1', img: 'images/jenny.png' },
         { name: 'card2', img: 'images/drake.png' },
         { name: 'card2', img: 'images/drake.png' },
         { name: 'card3', img: 'images/fine.png' },
